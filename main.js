6155dc4cfb98a9dca186794f0c770a12a65ebbfa
Nuel Makara <nuelmakara@gmail.com>
2018-07-25 15:06:40 -0400

    Cleaned the dollar sign selector files that werent working

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-alert("Welcome to Makara\'s Quiz Ninja");
-const question = "What is Superman's real name?";
-const answer = prompt(question);
-alert("You answered "+ answer);
-//Two variables were created question & answer
-//question holds the quiz question the user is to provide and answer to.
-//answer, the other variable fires a prompt with the embedded answer that the user chose!
+//Sets the questions in an array!
 const quiz = [
 	["What is SUperman's real name?", "Clark Kent"],
 	["What is Wonder Woman's real name?", "Diana Prince"],
 	["What is Batman's real name?", "Bruce Wayne"]
 	];
-let score = 0; // initialize score
-
-for(const [question, answer] of quiz){
+// sets score and initialize it!
+let score = 0;
+for(const [question, answer] of quiz.entries()){
+	
+	//Get response to questions from player
 	const response = prompt(question);
 	if(response === answer){
 		alert("Correct!");
+		//increment the score count by 1
 		score++;
 	} else {
-		alert('Wrong! The correct answer was'+ answer);
+		alert('Wrong! The correct answer was ${answer}');
 	}
 }
 //At the end of the game, report the player's score
-alert("Game Over, you scored "+score point+ (score !== 1) ? 's' : '');
+alert("Game Over, you scored ${score} point${score > 1 ? 's' : ''}");
