91ae47e020f9fb4f14790901609d1e70863c9f6a
NuelMakara <nuelmakara@gmail.com>
2019-03-15 03:30:01 -0700

    Backticks & typos correction!
    The interpolation symbols; backticks were placed in the places where quote marks were used, and a typographical error was corrected.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //Sets the questions in an array!
 const quiz = [
-	["What is SUperman's real name?", "Clark Kent"],
+	["What is Superman's real name?", "Clark Kent"],
 	["What is Wonder Woman's real name?", "Diana Prince"],
 	["What is Batman's real name?", "Bruce Wayne"]
+	["What is the name of United State's 45th president?", "Donald Trump"]
 	];
 // sets score and initialize it!
 let score = 0;
@@ -15,8 +16,8 @@ for(const [question, answer] of quiz.entries()){
 		//increment the score count by 1
 		score++;
 	} else {
-		alert('Wrong! The correct answer was ${answer}');
+		alert(`Wrong! The correct answer was ${answer}`);
 	}
 }
 //At the end of the game, report the player's score
-alert("Game Over, you scored ${score} point${score > 1 ? 's' : ''}");
+alert(`Game Over, you scored ${score} point${score > 1 ? 's' : ''}`);
